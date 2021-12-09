15c5d86a283c99e2f099ed91d5919555b5ddddcb
Yajindra <yajindragtm@gmail.com>
2021-12-09 21:52:47 +0545

    Fixed Edit page

--- Readme.md
+++ Readme.md
@@ -1,1 +1,11 @@
 # Story Book Wepapp Wih Google Login
+
+## Features Of Story Book Webapp
+
+1. **Google Authentication and Registation/Login Feature**
+2. **User Can Register and Login Using Their Google Account**
+3. **Create Story**
+4. **Make Their Story Public and Private. Public story can view all user but private are only accable to user who carete that story**
+5. **Update Stories**
+6. **Delete Stories**
+7. **Dashbord which shows all stories creade by user**
--- routes/stories.js
+++ routes/stories.js
@@ -71,22 +71,23 @@ router.get("/edit/:id", ensureAuth, async (req, res) => {
 router.put("/:id", ensureAuth, async (req, res) => {
   try {
     let story = await Story.findById(req.params.id).lean();
-    // Check Story Availibility
+
     if (!story) {
       return res.render("error/404");
     }
-    // Check User Auth
+
     if (story.user != req.user.id) {
       res.redirect("/stories");
     } else {
-      story = await Story.findOneAndUpdate({ _id: res.params.id }, req.body, {
+      story = await Story.findOneAndUpdate({ _id: req.params.id }, req.body, {
         new: true,
         runValidators: true,
       });
+
       res.redirect("/dashbord");
     }
-  } catch (error) {
-    console.log(error);
+  } catch (err) {
+    console.error(err);
     return res.render("error/500");
   }
 });
